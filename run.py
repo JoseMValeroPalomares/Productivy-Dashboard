5e89b6b45976d53e763304a9d59e426df2af85a3
JoseMValeroPalomares <121754710+JoseMValeroPalomares@users.noreply.github.com>
2025-09-07 16:48:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,3 @@ from config import ProductionConfig
 
 app = create_app(ProductionConfig)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)  # '0.0.0.0' escucha en todas las IPs
